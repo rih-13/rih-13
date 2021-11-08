3a018880f2537bfa925c13978910140252d378ff
rih-13 <93894242+rih-13@users.noreply.github.com>
2021-11-08 09:49:56 -0500

    Update essay calculator.py

--- essay calculator.py
+++ essay calculator.py
@@ -1,6 +1,5 @@
 ## This calculator helps you plan out an essay
 
-
 word = input("Pick a word")
 if len(word) % 2 == 0:
     print("Your word has an even amount of letters")
@@ -11,29 +10,19 @@ else:
 
 word_count = int(input("What is your character count? "))
 phrase = input("Type your text here for character count: ")
-if len(word) > word_count:
+if len(phrase) > word_count:
     print("Your text meets the character count")
 else:
     print("Your text is below the character count. ")
-    actual_word_count = (len(phrase))
-    remainder = abs(word_count - actual_word_count)
+    remainder = abs(word_count - len(phrase))
     print("You have " + str(remainder) + " characters left.")
-
-#We tell them how many they have left to meet
-
-#This part helps plan out the time needed
-
-if len(word) < word_count:
     time_spent = int(input('How many minutes have you spent on it so far? '))
-    rate = time_spent/actual_word_count
+    rate = time_spent / (len(phrase))
     time_needed = rate * remainder
     print("You need about " + str(time_needed) + " minutes left to complete it at this rate.")
-
-days_left = int(input("How many days left do you have to finish it? "))
-if days_left < 1:
-    print("lol")
-else:
-    time_perday = time_needed/days_left
-    print("You will need to spend at least " + str(time_perday) + " minutes every day to meet the deadline.")
-    
-
+    days_left = int(input("How many days left do you have to finish it? "))
+    if days_left <= 1:
+        print("lol")
+    else:
+        time_perday = time_needed / days_left
+        print("You will need to spend at least " + str(time_perday) + " minutes every day to meet the deadline.")
